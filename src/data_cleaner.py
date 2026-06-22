af6f735b3a45852a053a1275fe37d4861845e6a2
alinooralian <alinooralian.85@gmail.com>
2026-06-22 20:56:45 +0330

    Completing the imputer section

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,21 +1,29 @@
 from abc import ABC, abstractmethod
+import numpy as np
+import pandas as pd
+from sklearn.impute import KNNImputer
 
 class BaseImputer(ABC):
     @abstractmethod
-    def impute():
+    def impute(self, df: pd.DataFrame):
         pass
 
-class MeanImputer(BaseImputer):
-    def impute():
-        pass
 
-class MedianImputer(BaseImputer):
-    def impute():
-        pass
+class MeanDataImputer(BaseImputer):
+    def impute(self, df: pd.DataFrame):
+        return df.fillna(df.mean(numeric_only=True))
 
-class KNNImputer(BaseImputer):
-    def impute():
-        pass
+
+class MedianDataImputer(BaseImputer):
+    def impute(self, df: pd.DataFrame):
+        return df.fillna(df.median(numeric_only=True))
+
+
+class KNNDataImputer(BaseImputer):
+    def impute(self, df: pd.DataFrame, k=5):
+        imputer = KNNImputer(n_neighbors=k)
+
+        return pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
 class BaseOutlierHandler(ABC):
     @abstractmethod
