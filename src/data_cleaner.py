d67c52b089c9b4be236781379d649aafc49a7f95
alinooralian <alinooralian.85@gmail.com>
2026-06-22 20:58:38 +0330

    Completing the outliers section

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -2,7 +2,9 @@ from abc import ABC, abstractmethod
 import numpy as np
 import pandas as pd
 from sklearn.impute import KNNImputer
+from scipy.stats import zscore
 
+#Missing Values
 class BaseImputer(ABC):
     @abstractmethod
     def impute(self, df: pd.DataFrame):
@@ -25,15 +27,41 @@ class KNNDataImputer(BaseImputer):
 
         return pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
+# Outlier Values
 class BaseOutlierHandler(ABC):
     @abstractmethod
-    def handle():
+    def handle(self, df: pd.DataFrame):
         pass
+
 
 class IQROutlierHandler(BaseOutlierHandler):
-    def handle():
-        pass
+    def handle(self, df: pd.DataFrame, factor=1.5):
+        numeric_cols = df.select_dtypes("number").columns
+
+        for col in numeric_cols:
+            Q1 = df[col].quantile(0.25)
+            Q3 = df[col].quantile(0.75)
+            IQR = Q3 - Q1
+
+            lower_bound = Q1 - IQR * factor
+            upper_bound = Q3 + IQR * factor
+
+            mask = (df[col] < lower_bound) | (df[col] > upper_bound)
+
+            df.loc[mask, col] = np.nan
+
+        return df
+
 
 class ZScoreOutlierHandler(BaseOutlierHandler):
-    def handle():
-        pass
+    def handle(self, df: pd.DataFrame, threshold=3):
+        numeric_cols = df.select_dtypes("number").columns
+
+        for col in numeric_cols:
+            z_score = np.abs(zscore(df[col], nan_policy="omit"))
+
+            mask = z_score > threshold
+
+            df.loc[mask, col] = np.nan
+
+        return df
