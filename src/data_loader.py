07d3ba0d303198dc8003ce5889d12385f4b8ffcf
alinooralian <alinooralian.85@gmail.com>
2026-06-22 21:04:44 +0330

    Edit the data loader module

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,44 +1,68 @@
+import pandas as pd
 import csv
-from song import Song
+from src.song import Song
 
 
 class DataLoader:
     songs = []
 
-    def read_data_from_csv(self):
+    def __init__(self):
         with open("./data/dataset.csv", "r", encoding="utf-8") as f:
-            lines = csv.DictReader(f)
+            self.df = pd.read_csv(f, usecols=lambda col: col != "Unnamed: 0")
 
-            for line in lines:
-                line.pop("")
-                song = Song(line)
+    def missing_value_report(self):
+        check = False
+        cols = self.df.columns
 
-                self.songs.append(song)
+        for col in cols:
+            bool_series = pd.isnull(self.df[col])
+            missing_rows = self.df[bool_series]
 
-    def append_song(self, song):
+            if missing_rows.empty:
+                continue
+
+            print(f"Missing Vlues in {col}\n")
+            print(missing_rows)
+            print("\n\n\n")
+
+            check = True
+
+        return check
+
+    def create_song_obj(self):
+        self.songs.clear()
+
+        for _, line in self.df.iterrows():
+            song = Song(dict(line))
+            self.songs.append(song)
+
+    def append_song(self, song: Song):
         self.songs.append(song)
 
         with open("./data/dataset.csv", "a", encoding="utf-8") as f:
-            csv.writer.writrow(
-                len(self.songs) - 1,
-                song._track_id,
-                song._artists,
-                song._album_name,
-                song._track_name,
-                song._popularity,
-                song._duration_ms,
-                song._explicit,
-                song._danceability,
-                song._energy,
-                song._key,
-                song._loudness,
-                song._mode,
-                song._speechiness,
-                song._acousticness,
-                song._instrumentalness,
-                song._liveness,
-                song._valence,
-                song._tempo,
-                song._time_signature,
-                song._track_genre,
+            writer = csv.writer(f)
+
+            writer.writerow(
+                [
+                    song.track_id,
+                    song.artists,
+                    song.album_name,
+                    song.track_name,
+                    song.popularity,
+                    song.duration_ms,
+                    song.explicit,
+                    song.danceability,
+                    song.energy,
+                    song.key,
+                    song.loudness,
+                    song.mode,
+                    song.speechiness,
+                    song.acousticness,
+                    song.instrumentalness,
+                    song.liveness,
+                    song.valence,
+                    song.tempo,
+                    song.time_signature,
+                    song.track_genre,
+                ]
             )
